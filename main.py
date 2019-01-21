08129ec2218fc6864bab5b9c2af41c63b71ae6b5
Ryan Ratajczyk <polishponcho@gmail.com>
2019-01-20 21:50:53 -0600

    signup HTML and Login features

---
+++ main.py
@@ -0,0 +1,37 @@
+from flask import Flask, request, redirect, render_template, session
+
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+
+@app.route("/")
+def index():
+    
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        username_error = ''
+        password_error = ''
+        user = User.query.filter_by(username=username).first()
+        if user != None:
+            if user.password != password:
+                password_error = 'Invalid password'
+        if user == None:
+            username_error = 'Username does not exist'
+                
+        if user and user.password == password:
+            session['username'] = username
+            return redirect('/home')
+        else:
+            return render_template('login.html', username_error=username_error, password_error=password_error)
+
+    return render_template('login.html')
+
+
+@app.route('/signup') 
+def signup():
+
+    return render_template('signup.html')
+
+if __name__ == '__main__':
+    app.run(threaded = True)
